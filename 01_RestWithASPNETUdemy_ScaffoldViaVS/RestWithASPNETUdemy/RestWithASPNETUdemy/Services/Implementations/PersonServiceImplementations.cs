51a61082a47bf7334612b0a4f45fc41ce436cd27
KaueFulgencio <Kauemfulgencio@outlook.com>
2023-02-28 10:03:02 -0300

    third commit

---
+++ 01_RestWithASPNETUdemy_ScaffoldViaVS/RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementations/PersonServiceImplementations.cs
@@ -0,0 +1,74 @@
+﻿using RestWithASPNETUdemy.Model;
+
+namespace RestWithASPNETUdemy.Services.Implementations
+{
+    public class PersonServiceImplementations : IPersonService
+    {
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(Person person)
+        {
+            
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> persons = new List<Person>();
+            for(int i = 0; i < 8; i++)
+            {
+                Person person = MockPerson(i);
+                persons.Add(person);
+            }
+            return persons;
+        }
+
+        public Person FindByID(long id)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Kauana",
+                LastName = "Costa",
+                Address = "Santa Helena - Parana - Brasil",
+                Gender = "Female"
+            };
+
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+
+                FirstName = "Person Name" + i,
+
+                LastName = "Person Last Name" + i,
+
+                Address = "Some Address" + i,
+
+                Gender = "Female" + i
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+
+        void IPersonService.Delete(long id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 01_RestWithASPNETUdemy_ScaffoldViaVS/RestWithASPNETUdemy/Services/IPersonService.cs
@@ -0,0 +1,20 @@
+﻿using RestWithASPNETUdemy.Model;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace RestWithASPNETUdemy.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+
+        Person FindByID(long id);
+
+        List<Person> FindAll();
+
+        Person Update(Person person);
+
+        void Delete(long id);
+
+    }
+}
